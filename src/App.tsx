6de48cd2ac903be3009352406c88e28d07c6a01d
emertola <emertola@gmail.com>
2024-05-24 14:29:57 +0800

    feat: mark a todo item as completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 import { TODOS } from './data/todos.const';
+import { TodoItem as TodoListItem } from './types/todo.interface';
 
 function App() {
+  const [todos, setTodos] = useState<TodoListItem[]>(TODOS);
+
+  const setTodoCompleted = (id: number, completed: boolean) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+  };
+
   return (
     <>
       <main className="py-10 h-screen space-y-5">
         <h1 className="text-4xl font-semibold text-center">Todos:</h1>
         <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-8">
           <div className="space-y-2">
-            {TODOS.map((todo) => (
-              <TodoItem todo={todo} />
+            {todos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                onCompletedChange={() =>
+                  setTodoCompleted(todo.id, !todo.completed)
+                }
+              />
             ))}
           </div>
         </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,13 +2,19 @@ import { TodoItem as Todo } from '../types/todo.interface';
 
 interface TodoItemProps {
   todo: Todo;
+  onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-const TodoItem = ({ todo }: TodoItemProps) => {
+const TodoItem = ({ todo, onCompletedChange }: TodoItemProps) => {
   return (
     <div>
       <label className="flex items-center gap-2 border rounded-lg p-3 border-gray-300 bg-white hover:bg-slate-50">
-        <input type="checkbox" className="scale-125" />
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+          className="scale-125"
+        />
         <span className={todo.completed ? 'line-through text-gray-400' : ''}>
           {todo.title}
         </span>
