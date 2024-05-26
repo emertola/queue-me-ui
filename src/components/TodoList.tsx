6c24658f3a7559bdd0e42ba4b5fa24434e6a3861
emertola <emertola@gmail.com>
2024-05-26 18:35:35 +0800

    feat: sort todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,9 +8,16 @@ interface TodoListProps {
 }
 
 const TodoList = ({ todos, setTodoCompleted, onDelete }: TodoListProps) => {
+  const todosSorted = todos.sort((a, b) => {
+    if (a.completed === b.completed) {
+      return b.id - a.id;
+    }
+    return a.completed ? 1 : -1;
+  });
+
   return (
     <div className="space-y-2">
-      {todos.map((todo) => (
+      {todosSorted.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
